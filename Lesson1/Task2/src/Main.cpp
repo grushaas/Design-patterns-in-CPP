065f4dc65610553870a8fcfbcde56f514bbc6eef
Grushaas <grusha-as@yandex.ru>
2023-05-15 16:24:11 +0500

    Lesson1 - Task 2 - Version 1.0

---
+++ Lesson1/Task2/src/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "../include/SqlSelectQuery.h"
+
+int main()
+{
+	SqlSelectQueryBuilder queryBuilder;
+
+	queryBuilder.AddColumn("name").AddColumn("phone");
+	queryBuilder.AddFrom("students");
+	queryBuilder.AddWhere("id", "42").AddWhere("name", "John");
+
+	std::cout << queryBuilder.BuildQuery() << std::endl;
+
+	return 0;
+}
